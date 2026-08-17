954de4ab273f3cdcfd903e652527130e9e57ddaa
Josué Rodrigo Rico Arreola <josue.rico@uabc.edu.mx>
2026-08-16 21:38:26 -0700

    La clase Main fue modificada para iniciar la aplicación

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import Controlador.Controlador;
+import Vista.Vista;
+import Modelo.*;
 
-void main() {
-    //Esta linea va antes de cargar datos para evitar duplicados
-        //inventario.clear();
+public class Main {
+    public static void main(String[] args) {
+        Vista vista = new Vista();
+        ArchivoRegistro archivo = new ArchivoRegistro();
+        GestorInstrumentos gestor = new GestorInstrumentos();
+        Controlador controlador = new Controlador(vista, archivo, gestor);
+        controlador.iniciar();
     }
 
+}
